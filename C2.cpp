1644cd543eef3c952bed628f17807997d85945f2
HarshadWaman <156565915+HarshadWaman@users.noreply.github.com>
2025-01-09 16:53:30 +0530

    Create C2.cpp

---
+++ C2.cpp
@@ -0,0 +1,17 @@
+// write a c++ program to write a two number addition
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "enter first no: ";
+    cin >> a;
+
+    cout << "Enter second no: ";
+    cin >> b;
+    c = a + b;
+
+    cout << "Addition is: " << c;
+
+    return 0;
+}
